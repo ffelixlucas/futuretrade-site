41be8fdb300715027a858aa9e0cd397bf2a875a7
Lucas Felix <lucas.fafx@gmail.com>
2025-02-07 11:22:23 -0300

    fix: adicona area de depoiementos

---
+++ js/testimonial.js
@@ -0,0 +1,12 @@
+var swiper = new Swiper(".testimonial-swiper", {
+    loop: true,
+    autoplay: {
+      delay: 7000,
+      disableOnInteraction: false,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+  
